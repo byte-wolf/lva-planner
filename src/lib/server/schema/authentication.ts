d0c06547b3edd2deaa6361360f3c973f1996e076
Simon Wolffhardt <s.wolffhardt@protonmail.com>
2024-08-31 18:50:01 +0200

    add username and password

--- src/lib/server/schema/authentication.ts
+++ src/lib/server/schema/authentication.ts
@@ -2,7 +2,12 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const userTable = sqliteTable("user", {
     id: text("id").notNull().primaryKey(),
+    username: text("username").notNull().unique(),
+    password_hash: text("password_hash"),
 });
+
+export type User = typeof userTable.$inferSelect;
+export type InsertUser = typeof userTable.$inferInsert;
 
 export const sessionTable = sqliteTable("session", {
     id: text("id").notNull().primaryKey(),
